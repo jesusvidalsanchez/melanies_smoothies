9612f4b7a7ea968a9308d39e2158b983c359c302
jesusvidalsanchez <92474325+jesusvidalsanchez@users.noreply.github.com>
2026-03-24 11:16:03 +0100

    Fix JSON response handling for smoothie API

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,6 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
-
-
 # Write directly to the app
 st.title(f"Customize Your Smoothie! :cup_with_straw: ")
 st.write(
@@ -48,7 +46,7 @@ if ingredients_list:
 
 import requests  
 smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
-st.text(smoothiefroot_response)
+st.text(smoothiefroot_response.json()
 
 
 
