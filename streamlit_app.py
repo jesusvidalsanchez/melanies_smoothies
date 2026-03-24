a291716c316146212b0fd22f8059976f81c674ab
jesusvidalsanchez <92474325+jesusvidalsanchez@users.noreply.github.com>
2026-03-24 11:14:27 +0100

    Fetch watermelon data from external API
    Added functionality to fetch and display watermelon data from an external API.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,8 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 
+
+
 # Write directly to the app
 st.title(f"Customize Your Smoothie! :cup_with_straw: ")
 st.write(
@@ -44,7 +46,9 @@ if ingredients_list:
         
         st.success('Your Smoothie is ordered!', icon="✅")
 
-
+import requests  
+smoothiefroot_response = requests.get("[https://my.smoothiefroot.com/api/fruit/watermelon](https://my.smoothiefroot.com/api/fruit/watermelon)")  
+st.text(smoothiefroot_response)
 
 
 
